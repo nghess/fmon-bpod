6d7a732243ec4760275775ee3dd609433820f287
unknown <nhess@uoregon.edu>
2023-04-20 17:04:03 -0700

    bpod backup

--- fmos.m
+++ fmos.m
@@ -1,25 +1,38 @@
+%{
+FMOS MODULE for Bpod
+Preferences (Set variables in Freely Moving Olfactory Search Task)
+
+Written By: Nate Gonzales-Hess (nhess@uoregon.edu)
+Last Updated:
+%}
+
 function fmos
 %%
 global BpodSystem
-%A = BpodAnalogIn(BpodSystem.ModuleUSB.AnalogIn1);
 
 %% Set Reward amounts
 LeftValveTime = .17; %In practice, these will be defined by calibrate_h2o
 RightValveTime = .27;
 
+%% Build ITI list
+min_iti = 1;
+max_iti = 5;
+iti_list = round((max_iti-min_iti).*rand(1,150) + min_iti);
+
 %% Define parameters
+% check this and see how much is necessarry
 S = BpodSystem.ProtocolSettings; % Load settings chosen in launch manager into current workspace as a struct called S
 if isempty(fieldnames(S))  % If settings file was an empty struct, populate struct with default settings
     S.GUI.CurrentBlock = 1; % Training level % 1 = Direct Delivery at both ports 2 = Poke for delivery
-    S.GUI.RewardAmount = 5; %ul
+    S.GUI.RewardAmount = 5;  % ul
     S.GUI.PortOutRegDelay = 0.5; % How long the mouse must remain out before poking back in
 end
 
 %% Define trials
-nLeftTrials = 0;
-nRightTrials = 0;
-nOmissionTrials = 0;
+nLeftTrials = 0;%.45;
+nRightTrials = 0;%.45;
 nRandomTrials = 100;
+nOmissionTrials = round(nRandomTrials*.1);
 MaxTrials = nLeftTrials+nRightTrials+nRandomTrials;
 TrialTypes = [ones(1,nLeftTrials) ones(1,nRightTrials)*2 ceil(rand(1,nRandomTrials)*2)];
 BpodSystem.Data.TrialTypes = []; % The trial type of each trial completed will be added here.
@@ -28,7 +41,7 @@ BpodSystem.Data.TrialTypes = []; % The trial type of each trial completed will b
 BpodSystem.ProtocolFigures.OutcomePlotFig = figure('Position', [50 540 1000 250],'name','Outcome plot','numbertitle','off', 'MenuBar', 'none', 'Resize', 'off');
 BpodSystem.GUIHandles.OutcomePlot = axes('Position', [.075 .3 .89 .6]);
 TrialTypeOutcomePlot(BpodSystem.GUIHandles.OutcomePlot,'init',TrialTypes);
-BpodNotebook('init'); % Initialize Bpod notebook (for manual data annotation)
+%BpodNotebook('init'); % Initialize Bpod notebook (for manual data annotation)
 BpodParameterGUI('init', S); % Initialize parameter GUI plugin
 
 %% Main trial loop
@@ -37,11 +50,27 @@ for currentTrial = 1:MaxTrials
     switch TrialTypes(currentTrial) % Determine trial-specific state matrix fields
         case 1
             StateOnInitPoke = 'GoLeft';
+            OdorantState = 'OdorLeft';
         case 2
             StateOnInitPoke = 'GoRight'; 
+            OdorantState = 'OdorRight';
+        case 3
+            StateOnInitPoke = 'Omission'; 
+            OdorantState = 'OdorOmit';
     end
 
     sma = NewStateMachine(); % Initialize new state machine description
+    
+        
+    sma = AddState(sma, 'Name', 'Blank', ...
+    'Timer', .5,...
+    'StateChangeConditions', {'Tup', OpenOdorantValve},...
+    'OutputActions', {});
+    
+    %sma = AddState(sma, 'Name', 'OpenOdorantValve', ...
+    %'Timer', 0,...
+    %'StateChangeConditions', {'Port2In', StateOnInitPoke},...
+    %'OutputActions', {});
 
     sma = AddState(sma, 'Name', 'WaitForInitPoke', ...
     'Timer', 0,...
@@ -56,7 +85,12 @@ for currentTrial = 1:MaxTrials
     sma = AddState(sma, 'Name', 'GoRight', ...
     'Timer', 0,...
     'StateChangeConditions', {'SoftCode1', 'NoReward', 'SoftCode3', 'CorrectRight'},...
-    'OutputActions', {}); 
+    'OutputActions', {});
+
+    sma = AddState(sma, 'Name', 'Omission', ...
+    'Timer', 0,...
+    'StateChangeConditions', {'SoftCode1', 'CorrectLeft', 'SoftCode3', 'CorrectRight'},...
+    'OutputActions', {});
 
     sma = AddState(sma, 'Name', 'CorrectLeft', ...
         'Timer', 0,...
@@ -68,10 +102,15 @@ for currentTrial = 1:MaxTrials
         'StateChangeConditions', {'Port1In', 'NoReward', 'Port3In', 'RightReward'},...
         'OutputActions', {'ValveModule1', 2 'ValveModule2', 255, 'ValveModule3', 255}); 
 
-    sma = AddState(sma, 'Name', 'NoReward', ... % add pseudorandom ITI into timer?
+    sma = AddState(sma, 'Name', 'NoReward', ... 
         'Timer', 0,...
-        'StateChangeConditions', {'Tup', 'exit'},...
+        'StateChangeConditions', {'Port1In', 'ITI', 'Port3In', 'ITI'},...
         'OutputActions', {}); 
+    
+    sma = AddState(sma, 'Name', 'ITI', ... 
+    'Timer', iti_list(currentTrial),...  % Pseudorandom ITI into timer.
+    'StateChangeConditions', {'Tup', 'exit'},...
+    'OutputActions', {}); 
 
     sma = AddState(sma, 'Name', 'LeftReward', ...
         'Timer', LeftValveTime,...
@@ -99,8 +138,8 @@ for currentTrial = 1:MaxTrials
     T.startTrial(sma)
     RawEvents = T.getTrialData;
     if ~isempty(fieldnames(RawEvents)) % If trial data was returned
-        BpodSystem.Data = AddTrialEvents(BpodSystem.Data,RawEvents); % Computes trial events from raw data
-        BpodSystem.Data = BpodNotebook('sync', BpodSystem.Data); % Sync with Bpod notebook plugin
+        BpodSystem.Data = AddTrialEvents(BpodSystem.Data,RawEvents); %  Computes trial events from raw data
+        %BpodSystem.Data = BpodNotebook('sync', BpodSystem.Data); % Sync with Bpod notebook plugin
         BpodSystem.Data.TrialSettings(currentTrial) = S; % Adds the settings used for the current trial to the Data struct (to be saved after the trial ends)
         BpodSystem.Data.TrialTypes(currentTrial) = TrialTypes(currentTrial); % Adds the trial type of the current trial to data
         UpdateOutcomePlot(TrialTypes, BpodSystem.Data);
@@ -110,6 +149,7 @@ for currentTrial = 1:MaxTrials
     if BpodSystem.Status.BeingUsed == 0
         return
     end
+    %disp(iti_list(currentTrial));
 end
 
 function UpdateOutcomePlot(TrialTypes, Data)
