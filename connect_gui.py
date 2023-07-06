57f5c5e8e29958ac914911559053e3eb7f7f4af6
unknown <nhess@uoregon.edu>
2023-07-06 08:57:58 -0700

    GUI v.9

---
+++ connect_gui.py
@@ -0,0 +1,23 @@
+import pyautogui
+import time
+import pygetwindow as gw
+
+# Get a specific window by its title
+bonsai = gw.getWindowsWithTitle('fmon-pid.bonsai')[0]
+bpod = gw.getWindowsWithTitle('Bpod Console')[0]
+
+# Activate the window
+bonsai.activate()
+bonsai.maximize()
+
+# Wait for the window to be active
+time.sleep(1)
+
+# Start Bonsai
+pyautogui.press('f5')
+
+# Wait for the window to be active
+time.sleep(1)
+
+# Activate the window
+bpod.activate()
